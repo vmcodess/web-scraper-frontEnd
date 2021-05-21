1986698e9ca58df34f6aff638ea99ff6517bb520
vmatveev <vmatveevspam@gmail.com>
2021-05-20 23:06:46 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import Signin from './components/Signin/Signin'
+import Navigation from './components/Navigation/Navigation'
+import Register from './components/Register/Register'
+import Home from './components/Home/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  // connect to backend
+  componentDidMount() {
+    fetch('http://localhost:5000')
+      .then(response => response.json())
+      .then(console.log)
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: 'true'})
+    }
+    this.setState({route: route})
+  }
+
+  render() {
+    const { isSignedIn, route } = this.state;
+     return (
+      <div >
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home' 
+          ? <Home /> 
+          : (
+            route === 'signin' 
+            ? <Signin onRouteChange={this.onRouteChange} /> 
+            : <Register /> 
+          )     
+        }
+      </div>
+    )
+  }
+ 
 }
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            You have signed in and are on homepage. Congratulations!
+        </div>
+    )
+}
+
+export default Home;
