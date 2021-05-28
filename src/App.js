a1c923e21f111e3642f69f6d3e8ca7306801cf6a
vmatveev <vmatveevspam@gmail.com>
2021-05-27 21:45:33 -0400

    need to add login error if wrong password

--- src/App.js
+++ src/App.js
@@ -1,52 +1,83 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+//import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Signin from './components/Signin/Signin'
 import Navigation from './components/Navigation/Navigation'
 import Register from './components/Register/Register'
 import Home from './components/Home/Home'
 
+const initialState = {
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+   // id: '',
+    name: '',
+    email: '',
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      route: 'signin',
-      isSignedIn: false
-    }
+    this.state = initialState;
   }
 
-  // connect to backend
-  componentDidMount() {
-    fetch('http://localhost:5000')
-      .then(response => response.json())
-      .then(console.log)
+  loadUser = (data) => {
+    this.setState({user: {
+      //id: data.id,
+      name: data.name,
+      email: data.email,
+    }})
   }
 
-  onRouteChange = (route) => {
-    if (route === 'signout') {
-      this.setState({isSignedIn: false})
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onRouteChange= (route) => {
+    if(route === 'signout') {
+      this.setState(initialState)
     } else if (route === 'home') {
-      this.setState({isSignedIn: 'true'})
+      this.setState({isSignedIn: true})
     }
-    this.setState({route: route})
+    this.setState({route: route});
   }
 
   render() {
     const { isSignedIn, route } = this.state;
-     return (
-      <div >
+    return (
+      // <div>
+      //   <BrowserRouter>
+      //     <Navigation />
+      //     <Switch>
+
+      //       {/* <Route exact path="/" component={Home} /> */}
+      //       <Route exact path="/" component={Signin} />
+      //       {/* <Route path="/signin" component={Signin} /> */}
+      //       <Route path="/home" component={Home} />
+      //       <Route path="/register" component={Register} />
+      //       <Route component={Error}></Route> // Error Page
+
+      //     </Switch>
+      //   </BrowserRouter>
+      // </div>
+
+      //---------------------------
+
+      <div>
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        { route === 'home' 
-          ? <Home /> 
+        {
+          route ==='home'
+          ? <Home />
           : (
-            route === 'signin' 
-            ? <Signin onRouteChange={this.onRouteChange} /> 
-            : <Register /> 
-          )     
+            route === 'signin'
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          )
         }
       </div>
     )
   }
- 
 }
 
 export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
     if (isSignedIn) {
         return (
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <p onClick={()=> onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign out</p>
+                <p onClick= {() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign out</p>
             </nav>
         )
     } else {
         return (
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <p onClick={()=> onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
-                <p onClick={()=> onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+                <p onClick= {() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign in</p>
+                <p onClick= {() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
             </nav>
         );
     }
 }
+
+// const Navigation = () => {
+//     return (
+//         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+//             <Link to="/signin" className='f3 link dim black underline pa3 pointer'>Sign in</Link>
+//             <Link to="/register" className='f3 link dim black underline pa3 pointer'>Register</Link>
+//         </nav> 
+//     )
+// }
 
 export default Navigation;
