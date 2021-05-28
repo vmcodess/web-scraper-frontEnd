d1693d97375d051395eea346b65bcf53fcef03e7
vmatveev <vmatveevspam@gmail.com>
2021-05-27 21:47:15 -0400

    removed extra comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Signin from './components/Signin/Signin'
 import Navigation from './components/Navigation/Navigation'
 import Register from './components/Register/Register'
@@ -9,7 +8,6 @@ const initialState = {
   route: 'signin',
   isSignedIn: false,
   user: {
-   // id: '',
     name: '',
     email: '',
     joined: ''
@@ -24,7 +22,6 @@ class App extends Component {
 
   loadUser = (data) => {
     this.setState({user: {
-      //id: data.id,
       name: data.name,
       email: data.email,
     }})
@@ -46,24 +43,6 @@ class App extends Component {
   render() {
     const { isSignedIn, route } = this.state;
     return (
-      // <div>
-      //   <BrowserRouter>
-      //     <Navigation />
-      //     <Switch>
-
-      //       {/* <Route exact path="/" component={Home} /> */}
-      //       <Route exact path="/" component={Signin} />
-      //       {/* <Route path="/signin" component={Signin} /> */}
-      //       <Route path="/home" component={Home} />
-      //       <Route path="/register" component={Register} />
-      //       <Route component={Error}></Route> // Error Page
-
-      //     </Switch>
-      //   </BrowserRouter>
-      // </div>
-
-      //---------------------------
-
       <div>
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         {
